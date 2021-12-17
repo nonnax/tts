55a6c40a879d17d2855eba5c40545a19349fba8d
nonnax <ironald@gmail.com>
2021-12-17 19:04:48 +0800

    update: mp3time.rb

--- mp3time.rb
+++ mp3time.rb
@@ -3,27 +3,29 @@
 
 # Id$ nonnax 2021-10-07 21:15:22 +0800
 require 'csv'
-require 'rubytools' 
+require 'rubytools'
 require 'core_ext'
 require 'array_table'
-require 'thread_ext'
 
-times = []
-list=[]
+list = []
 Dir['*.mp3'].each do |f|
-  Thread.new(times, list) do |times, list|
-    t = IO.popen("exiftool '#{f}'", &:readlines)
-    next unless t
-    t = t.grep(/Duration/).first.split(/:/, 2).last.scan(/\S+/).first 
-    ts = "0#{t.strip.gsub(/:/, '')}".to_f*1000
-    ms = ts.to_i
-    # list<<[f, (ts/1000).commify, ms.to_ts]
-    puts [f, (ts/1000).commify, ms.to_ts].to_csv
-    times<<ms
-    rescue=>e
-    # p e
-    next
-  end.join
+  t = IO.popen("exiftool '#{f}'", &:readlines)
+  next unless t
+
+  t =
+    t.grep(/Duration/)
+     .first
+     .split(/:/, 2)
+     .last
+     .scan(/\S+/)
+     .first
+  ts = "0#{t.strip.gsub(/:/, '')}".to_f * 1000
+  ms = ts.to_i
+  times << ms
+
+  puts [f, (ts / 1000).commify, ms.to_ts].to_csv
+rescue  StandardError => e
+  next
 end
 
-puts [:Total_time, (times.sum/1000.0).commify, times.sum.to_ts].to_csv
+puts [:Total_time, (times.sum / 1000.0).commify, times.sum.to_ts].to_csv
